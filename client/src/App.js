07c474239b5dbb28a1a674335da7f71cf8f7a71f
Paul Levent <paullevent@MacBook-Pro-de-Paul.local>
2020-12-27 18:57:33 +0100

    abc

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+
+  useEffect(() => {
+    axios.get('/api/sayhi')
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
+  })
+
   return (
     <div className="App">
       <header className="App-header">
