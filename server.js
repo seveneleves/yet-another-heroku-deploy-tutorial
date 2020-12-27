cb552b1c0ff02dda91b8c5816a608664207d0ff2
Paul Levent <paullevent@MacBook-Pro-de-Paul.local>
2020-12-27 18:36:59 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // promiseLibrary: global.Promise
-});
+app.use(express.json());
+app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
