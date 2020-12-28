4afe16c5b9fe1c3a1973fe1ad5b80510815f9a7f
Paul Levent <paullevent@MacBook-Pro-de-Paul.local>
2020-12-28 14:13:22 +0100

    should I really deploy the app twice?

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/sayhi', (req, res) => {
-  res.send('Hello stranger!')
+  res.send('Welcome stranger!')
 })
 
 module.exports = router;
